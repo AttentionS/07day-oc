aef3c515c840414f7fe3153050cfc70102026e66
Attention Liu <yuningsong@yuningdemacbook-air.local>
2016-03-14 13:45:29 +0800

    Initial Commit

---
+++ 07day-oc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  07day-oc
+//
+//  Created by Attention Liu on 16/3/14.
+//  Copyright © 2016年 Attention Liu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
